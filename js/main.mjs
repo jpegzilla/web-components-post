c67298715cdff662b716c1eafb2190adb6434f1e
jpegzilla <eris@jpegzilla.com>
2020-10-06 13:43:18 -0400

    added clock import and registered it

--- js/main.mjs
+++ js/main.mjs
@@ -1,1 +1,7 @@
-import Clock from "./components/clock.mjs";
+import Clock from './components/clock.mjs'
+
+const elements = [Clock]
+
+elements.forEach(({ name, element }) => {
+  customElements.define(name, element)
+})
