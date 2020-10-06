a263989c77e56767c00af26c5a3252ec0f336c40
jpegzilla <eris@jpegzilla.com>
2020-10-06 13:44:04 -0400

    added completed clock element

--- js/components/clock.mjs
+++ js/components/clock.mjs
@@ -1,8 +1,26 @@
-import Component from "./component.mjs";
-import html from "./../utils/html.mjs";
+import Component from './component.mjs'
+import html from './../utils/html.mjs'
 
 class Clock extends Component {
-  connectedCallback() {}
+  constructor() {
+    super()
+
+    this.attachShadow({ mode: 'open' })
+  }
+
+  getTime() {
+    return new Date().toLocaleTimeString()
+  }
+
+  connectedCallback() {
+    this.shadowRoot.innerHTML = html`
+      <time id="clock" part="time">${this.getTime()}</time>
+    `
+
+    setInterval(() => {
+      this.shadowRoot.getElementById('clock').textContent = this.getTime()
+    }, 1000)
+  }
 }
 
-export default { element: Clock, name: "clock-element" };
+export default { element: Clock, name: 'clock-element' }
