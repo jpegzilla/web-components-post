b593bd122563b97d08dc3d18e3c7b033ebcd135c
jpegzilla <eris@jpegzilla.com>
2020-10-06 13:57:37 -0400

    removed pointless method

--- js/components/component.mjs
+++ js/components/component.mjs
@@ -1,27 +1,23 @@
 class Component extends HTMLElement {
   addClass(className) {
-    this.classList.add(className);
+    this.classList.add(className)
   }
 
   removeClass(className) {
-    this.classList.remove(className);
+    this.classList.remove(className)
   }
 
   toggleClass(className) {
-    this.classList.toggle(className);
+    this.classList.toggle(className)
   }
 
   setId(id) {
-    this.id = id;
-  }
-
-  removeAttribute(attr) {
-    this.removeAttribute(attr);
+    this.id = id
   }
 
   attr(name, val) {
-    return val ? this.setAttribute(name, val) : this.getAttribute(name);
+    return val ? this.setAttribute(name, val) : this.getAttribute(name)
   }
 }
 
-export default Component;
+export default Component
