3b186c7a5535df226c78fb28bba63bb80ff99f8d
jpegzilla <eris@jpegzilla.com>
2020-10-05 21:33:42 -0400

    added base clock component

---
+++ js/components/clock.mjs
@@ -0,0 +1,8 @@
+import Component from "./component.mjs";
+import html from "./../utils/html.mjs";
+
+class Clock extends Component {
+  connectedCallback() {}
+}
+
+export default { element: Clock, name: "clock-element" };
